075a1ab13199394105e93ff91d0360d9f1e9ac67
israrkhann08 <israrkhann08@gmail.com>
2025-03-24 03:56:06 +0500

    BREZA Brand website design

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+let a = 2;
+const b = 30;
+
+function nameFun (){
+    // let c = 40;
+    // const b = 40;
+  console.log(b);
+    
+    // console.log(a , b, "inside function");
+    
+}
+nameFun();
+
+
+// console.log(c);
